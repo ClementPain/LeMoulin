22773e238c08369058c37d16e6c15a37df42f54d
clement <clementpain@outlook.com>
2020-12-05 11:48:35 +0100

    set API for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.delete_all
+
+5.times do
+  user = User.create! email: Faker::Internet.email, password: 'password'
+end
