992ea4126ad7e67d926b862ab04b49fcbbda6910
Nazaninf24 <nazaninf24@gmail.com>
2020-12-07 17:06:31 +0100

    push

--- app/javascript/src/pages/Home/jumborton.js
+++ app/javascript/src/pages/Home/jumborton.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import bgimage from '../../../assets/images/Formation-Web.jpg'
+import React from 'react'
+import bgimage from '../../../../assets/images/commerces-de-proximite-lille.jpg'
 
 
 const Jumborton = ()  => (
 
-	<div className="jumbotron text-center" style={{ backgroundImage: `url(${bgimage})`, backgroundSize: 'cover' , backgroundPosition: 'center center'}}>
-			<h3 className="display-5 text-white" >Form-Yourself</h3>
-			<p className="lead text-white">FormYourself is a training sales company. Different companies buy a package from FormYourself. Thus, their employees can register for all the training they wish, without limit.</p>
-			<div className="my-4 text-white">
-			<p>To see more details about inscriptions and training formulas </p>
-			<p className="lead">
-				<a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-			</p>
-			</div>
-		</div>
-
+<div className="jumbotron text-center" style={{ backgroundImage: `url(${bgimage})`, backgroundSize: 'cover' , backgroundPosition: 'center center'}}>
+  <h1 className="display-3">Hello, world!</h1>
+  <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+  <div className="my-4">
+  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+  <p className="lead">
+    <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+  </p>
+</div>
+</div>
 )
 
-export default Jumborton
+export default Jumborton;
