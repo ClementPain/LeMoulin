43ea8abce74cb9b06efe8d6c00475c4416f7c91b
Nazaninf24 <nazaninf24@gmail.com>
2020-12-07 12:05:00 +0100

    Add

--- app/javascript/src/app.js
+++ app/javascript/src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'bootswatch/dist/lux/bootstrap.min.css'; 
-import './App.css';
+import './custom.scss';
 import {
   BrowserRouter as Router
 } from "react-router-dom";
