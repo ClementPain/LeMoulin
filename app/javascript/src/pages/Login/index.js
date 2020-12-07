0754ccda2e9ce9041836ac0569b26d48e990d82e
Nazaninf24 <nazaninf24@gmail.com>
2020-12-07 15:57:17 +0100

    Add

--- app/javascript/src/pages/Login/index.js
+++ app/javascript/src/pages/Login/index.js
@@ -4,16 +4,16 @@ import Auth from '../Auth';
 const Login = () => (
   <Auth type="login">
     <div className="form-group row">
-      <label htmlFor="email" className="col-md-5 text-md-right">
+      <label htmlFor="email" className="text-md-right">
         Email
       </label>
-      <input type="text" placeholder="Enter your email" name="email" />
+      <input type="text" placeholder="Enter your email"className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" />
     </div>
     <div className="form-group row">
-      <label htmlFor="password" className="col-md-5 text-md-right">
+      <label htmlFor="password" className="text-md-right">
         Password
       </label>
-      <input type="password" placeholder="Enter your password" name="password" />
+      <input type="password" placeholder="Enter your password" className="form-control" id="exampleInputPassword1" name="password" />
     </div>
   </Auth>
 );
