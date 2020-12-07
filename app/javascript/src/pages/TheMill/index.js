da666b27cc24bd46a6ea02d531c6691a7fbfa951
ouin85 <bouinten@yahoo.fr>
2020-12-07 22:41:00 +0100

    keep user authenticated when refresh the site

--- app/javascript/src/pages/TheMill/index.js
+++ app/javascript/src/pages/TheMill/index.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
 import Routes from '../../routes';
 import Navbar from '../../components/Navbar';
+import { authCookieHandler } from '../../tools';
+import { authSuccess } from '../../redux-config';
 
-const TheMill = () => (
-  <div className="container-fluid">
-    <Navbar />
-    <Routes />
-  </div>
-)
+const { getAuthCookie } = authCookieHandler;
+
+const TheMill = () => {
+  const dispatch = useDispatch();
+
+  const authUserIfAuthCookieExist = () => {
+    const authCookie = getAuthCookie();
+    if (authCookie) { dispatch(authSuccess(authCookie.currentUserId)); }
+  };
+  
+  useEffect(
+    () => { authUserIfAuthCookieExist(); },
+    [],
+    );
+    
+  return (
+    <div className="container-fluid">
+      <Navbar />
+      <Routes />
+    </div>
+  )
+}
 
 export default TheMill;
