9092ee1e38a846d7f2e09c2f50ee914693f12ef4
ouin85 <bouinten@yahoo.fr>
2020-12-09 18:33:47 +0100

    install eslint

--- app/javascript/src/pages/AboutUs/aboutus.jsx
+++ app/javascript/src/pages/AboutUs/aboutus.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import bgimage from '../../../../assets/images/commerces-de-proximite-lille.jpg'
 
 import { Jumbotron, Container } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
