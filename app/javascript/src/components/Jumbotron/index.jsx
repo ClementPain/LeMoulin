b04a22cc391ef5fd5b0ace04e179742a6aaae2a3
Nazaninf24 <nazaninf24@gmail.com>
2020-12-09 16:25:28 +0100

    add

--- app/javascript/src/components/Jumbotron/index.jsx
+++ app/javascript/src/components/Jumbotron/index.jsx
@@ -6,8 +6,8 @@ import Button from 'react-bootstrap/Button'
 
 const Hero = ()  => (
 
-<Jumbotron fluid >
-<div className="jumbotron text-center" style={{ backgroundImage: `url(${bgimage})`, backgroundSize: 'cover' , backgroundPosition: 'center center'}}>
+<Jumbotron fluid>
+<div className="bg  text-center" style={{ backgroundImage: `url(${bgimage})`, backgroundSize: 'cover' , backgroundPosition: 'center center'}}>
 <h1 className="display-4">LE MOULIN</h1>
 <p className="lead text-white">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
 <div className="my-4">
