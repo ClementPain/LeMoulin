a0d29a72ca4cbbddd493bf900b932587853e7bfc
ouin85 <bouinten@yahoo.fr>
2020-12-10 14:31:47 +0100

    ---

--- app/javascript/src/pages/Profile/index.jsx
+++ app/javascript/src/pages/Profile/index.jsx
@@ -6,7 +6,7 @@ import {
   Field, ErrorMessage, Form, Formik,
 } from 'formik';
 import {
-  Container, Row, Col, Card, Tabs, Tab, Button, FormGroup,
+  Container, Row, Col, Card, Tabs, Tab, Button, Accordion, FormGroup,
 } from 'react-bootstrap';
 
 import avatar from './avatar.png';
@@ -46,12 +46,112 @@ const Profile = () => {
           <Card>
             <Card.Body>
               <Card.Title>
+                User
                 {currentUser?.profile.first_name}
                 {' '}
                 {currentUser?.profile.last_name}
               </Card.Title>
+              <Card.Text />
+              <p>{currentUser?.profile.address}</p>
+              <p>{currentUser?.profile.zip_code}</p>
+              <p className="text-muted">{currentUser?.email}</p>
             </Card.Body>
           </Card>
+          <Accordion className="my-3">
+            <Card>
+              <Card.Header className="bg-primary">
+                <Accordion.Toggle as={Button} variant="link" eventKey="0" className="text-white text-decoration-none">
+                  Informations de connexion
+                </Accordion.Toggle>
+              </Card.Header>
+              <Accordion.Collapse eventKey="0">
+                <Card.Body>
+                  <Formik
+                    initialValues={{ email: '', password: '', passwordConfirmation: '' }}
+                    validate={(values) => {
+                      const formErrors = {};
+
+                      if (!values.email) {
+                        formErrors.email = 'Required';
+                      } else if (
+                        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                      ) {
+                        formErrors.email = 'Invalid email address';
+                      }
+
+                      if (!values.password) {
+                        formErrors.password = 'Required';
+                      } else if (values.password.length < 6) {
+                        formErrors.password = 'Must be 6 characters or more';
+                      }
+
+                      if (!values.passwordConfirmation) {
+                        formErrors.passwordConfirmation = 'Required';
+                      } else if (values.passwordConfirmation.length < 6) {
+                        formErrors.passwordConfirmation = 'Must be 6 characters or more';
+                      }
+
+                      if (!values.currentPassword) {
+                        formErrors.currentPassword = 'Required';
+                      } else if (values.currentPassword.length < 6) {
+                        formErrors.currentPassword = 'Must be 6 characters or more';
+                      }
+
+                      return formErrors;
+                    }}
+                    onSubmit={(values, { setSubmitting }) => {
+                      setSubmitting(false);
+                      handleSubmit(values);
+                    }}
+                  >
+                    {({ isSubmitting }) => (
+                      <Form onInput={handleOnInput}>
+                        { alert && alert.error && (<div className="alert alert-danger">{ alert.error }</div>) }
+                        <FormGroup>
+                          <Field name="email" type="email" placeholder="Email" className="form-control" />
+                          <ErrorMessage name="email" component="div" className="alert alert-danger" />
+                          { alert?.errors && alert.errors.email && (<div className="alert alert-danger">{alert.errors.email.join(', ')}</div>) }
+                        </FormGroup>
+
+                        <FormGroup>
+                          <Field name="password" type="password" placeholder="Mot de passe" className="form-control" />
+                          <ErrorMessage name="password" component="div" className="alert alert-danger" />
+                          { alert?.errors && alert.errors.password && (<div className="alert alert-danger">{alert.errors.password.join(', ')}</div>) }
+                        </FormGroup>
+
+                        <FormGroup>
+                          <Field name="passwordConfirmation" type="password" placeholder="Confirmation de mot de passe" className="form-control" />
+                          <ErrorMessage name="passwordConfirmation" component="div" className="alert alert-danger" />
+                          { alert?.errors && alert.errors.password && (<div className="alert alert-danger">{alert.errors.password.join(', ')}</div>) }
+                        </FormGroup>
+
+                        <FormGroup>
+                          <Field name="currentPassword" type="password" placeholder="Mot de passe actuel" className="form-control" />
+                          <ErrorMessage name="currentPassword" component="div" className="alert alert-danger" />
+                          { alert?.errors && alert.errors.password && (<div className="alert alert-danger">{alert.errors.password.join(', ')}</div>) }
+                        </FormGroup>
+                        <FormGroup className="text-center">
+                          <Button variant="primary" type="submit" disabled={isSubmitting}>
+                            Submit
+                          </Button>
+                        </FormGroup>
+                      </Form>
+                    )}
+                  </Formik>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card>
+              <Card.Header className="bg-primary">
+                <Accordion.Toggle as={Button} variant="link" eventKey="1" className="text-white text-decoration-none">
+                  Informations personnelles
+                </Accordion.Toggle>
+              </Card.Header>
+              <Accordion.Collapse eventKey="1">
+                <Card.Body>Informations personnelles</Card.Body>
+              </Accordion.Collapse>
+            </Card>
+          </Accordion>
         </Col>
         <Col md={9}>
           <Tabs defaultActiveKey="my-commands">
@@ -60,154 +160,6 @@ const Profile = () => {
             </Tab>
             <Tab eventKey="my-shop" title="Ma boutique" className="p-3">
               Ma boutique
-            </Tab>
-            <Tab eventKey="my-auth-info" title="Informations de connexion" className="p-3">
-              <Formik
-                initialValues={{ email: '', password: '', passwordConfirmation: '' }}
-                validate={(values) => {
-                  const formErrors = {};
-
-                  if (!values.email) {
-                    formErrors.email = 'Required';
-                  } else if (
-                    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-                  ) {
-                    formErrors.email = 'Invalid email address';
-                  }
-
-                  if (!values.password) {
-                    formErrors.password = 'Required';
-                  } else if (values.password.length < 6) {
-                    formErrors.password = 'Must be 6 characters or more';
-                  }
-
-                  if (!values.passwordConfirmation) {
-                    formErrors.passwordConfirmation = 'Required';
-                  } else if (values.passwordConfirmation.length < 6) {
-                    formErrors.passwordConfirmation = 'Must be 6 characters or more';
-                  }
-
-                  if (!values.currentPassword) {
-                    formErrors.currentPassword = 'Required';
-                  } else if (values.currentPassword.length < 6) {
-                    formErrors.currentPassword = 'Must be 6 characters or more';
-                  }
-
-                  return formErrors;
-                }}
-                onSubmit={(values, { setSubmitting }) => {
-                  setSubmitting(false);
-                  handleSubmit(values);
-                }}
-              >
-                {({ isSubmitting }) => (
-                  <Form onInput={handleOnInput}>
-                    { alert && alert.error && (<div className="alert alert-danger">{ alert.error }</div>) }
-                    <FormGroup>
-                      <Field name="email" type="email" placeholder="Email" className="form-control" />
-                      <ErrorMessage name="email" component="div" className="alert alert-danger" />
-                      { alert?.errors && alert.errors.email && (<div className="alert alert-danger">{alert.errors.email.join(', ')}</div>) }
-                    </FormGroup>
-
-                    <FormGroup>
-                      <Field name="password" type="password" placeholder="Mot de passe" className="form-control" />
-                      <ErrorMessage name="password" component="div" className="alert alert-danger" />
-                      { alert?.errors && alert.errors.password && (<div className="alert alert-danger">{alert.errors.password.join(', ')}</div>) }
-                    </FormGroup>
-
-                    <FormGroup>
-                      <Field name="passwordConfirmation" type="password" placeholder="Confirmation de mot de passe" className="form-control" />
-                      <ErrorMessage name="passwordConfirmation" component="div" className="alert alert-danger" />
-                      { alert?.errors && alert.errors.password && (<div className="alert alert-danger">{alert.errors.password.join(', ')}</div>) }
-                    </FormGroup>
-
-                    <FormGroup>
-                      <Field name="currentPassword" type="password" placeholder="Mot de passe actuel" className="form-control" />
-                      <ErrorMessage name="currentPassword" component="div" className="alert alert-danger" />
-                      { alert?.errors && alert.errors.password && (<div className="alert alert-danger">{alert.errors.password.join(', ')}</div>) }
-                    </FormGroup>
-                    <FormGroup className="text-center">
-                      <Button variant="primary" type="submit" disabled={isSubmitting}>
-                        Submit
-                      </Button>
-                    </FormGroup>
-                  </Form>
-                )}
-              </Formik>
-            </Tab>
-            <Tab eventKey="my-perso-info" title="Informations personnelles" className="p-3">
-              <Formik
-                initialValues={{ email: '', password: '', passwordConfirmation: '' }}
-                validate={(values) => {
-                  const formErrors = {};
-
-                  if (!values.email) {
-                    formErrors.email = 'Required';
-                  } else if (
-                    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-                  ) {
-                    formErrors.email = 'Invalid email address';
-                  }
-
-                  if (!values.password) {
-                    formErrors.password = 'Required';
-                  } else if (values.password.length < 6) {
-                    formErrors.password = 'Must be 6 characters or more';
-                  }
-
-                  if (!values.passwordConfirmation) {
-                    formErrors.passwordConfirmation = 'Required';
-                  } else if (values.passwordConfirmation.length < 6) {
-                    formErrors.passwordConfirmation = 'Must be 6 characters or more';
-                  }
-
-                  if (!values.currentPassword) {
-                    formErrors.currentPassword = 'Required';
-                  } else if (values.currentPassword.length < 6) {
-                    formErrors.currentPassword = 'Must be 6 characters or more';
-                  }
-
-                  return formErrors;
-                }}
-                onSubmit={(values, { setSubmitting }) => {
-                  setSubmitting(false);
-                  handleSubmit(values);
-                }}
-              >
-                {({ isSubmitting }) => (
-                  <Form onInput={handleOnInput}>
-                    { alert && alert.error && (<div className="alert alert-danger">{ alert.error }</div>) }
-                    <FormGroup>
-                      <Field name="email" type="email" placeholder="Email" className="form-control" />
-                      <ErrorMessage name="email" component="div" className="alert alert-danger" />
-                      { alert?.errors && alert.errors.email && (<div className="alert alert-danger">{alert.errors.email.join(', ')}</div>) }
-                    </FormGroup>
-
-                    <FormGroup>
-                      <Field name="password" type="password" placeholder="Mot de passe" className="form-control" />
-                      <ErrorMessage name="password" component="div" className="alert alert-danger" />
-                      { alert?.errors && alert.errors.password && (<div className="alert alert-danger">{alert.errors.password.join(', ')}</div>) }
-                    </FormGroup>
-
-                    <FormGroup>
-                      <Field name="passwordConfirmation" type="password" placeholder="Confirmation de mot de passe" className="form-control" />
-                      <ErrorMessage name="passwordConfirmation" component="div" className="alert alert-danger" />
-                      { alert?.errors && alert.errors.password && (<div className="alert alert-danger">{alert.errors.password.join(', ')}</div>) }
-                    </FormGroup>
-
-                    <FormGroup>
-                      <Field name="currentPassword" type="password" placeholder="Mot de passe actuel" className="form-control" />
-                      <ErrorMessage name="currentPassword" component="div" className="alert alert-danger" />
-                      { alert?.errors && alert.errors.password && (<div className="alert alert-danger">{alert.errors.password.join(', ')}</div>) }
-                    </FormGroup>
-                    <FormGroup className="text-center">
-                      <Button variant="primary" type="submit" disabled={isSubmitting}>
-                        Submit
-                      </Button>
-                    </FormGroup>
-                  </Form>
-                )}
-              </Formik>
             </Tab>
           </Tabs>
         </Col>
