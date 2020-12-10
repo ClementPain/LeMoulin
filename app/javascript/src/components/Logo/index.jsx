7460633b3a80eba2158b6199cdc9c15853a104b5
clement <clementpain@outlook.com>
2020-12-10 18:57:27 +0100

    delete scss logo

--- app/javascript/src/components/Logo/index.jsx
+++ app/javascript/src/components/Logo/index.jsx
@@ -1,9 +1,8 @@
-import './index.css';
 import React from 'react';
 import LogoImage from './Logo-Moulin-Negatif.png';
 
 const Logo = () => (
-  <img src={LogoImage} className="Logo" alt="Logo" />
+  <img src={LogoImage} className="Logo" alt="Logo" style={{height="70px"}} />
 );
 
 export default Logo;
