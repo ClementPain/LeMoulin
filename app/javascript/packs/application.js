4025791c2718cd5f8263629ae19bcd2174ba00ca
clement <clementpain@outlook.com>
2020-12-10 17:57:37 +0100

    solve issue bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ require("@rails/activestorage").start()
 require("channels")
 
 import 'bootstrap'
-import './src/application.scss'
+import '../src/application.scss'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
