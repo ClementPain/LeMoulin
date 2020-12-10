d9adcb3bf088f7c42ba917a27dbac786549e37d7
clement <clementpain@outlook.com>
2020-12-10 19:00:19 +0100

    style logo

--- app/javascript/src/components/Logo/index.jsx
+++ app/javascript/src/components/Logo/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import LogoImage from './Logo-Moulin-Negatif.png';
 
 const Logo = () => (
-  <img src={LogoImage} className="Logo" alt="Logo" style={{height="70px"}} />
+  <img src={LogoImage} className="Logo" alt="Logo" style={{height:"70px"}} />
 );
 
 export default Logo;
