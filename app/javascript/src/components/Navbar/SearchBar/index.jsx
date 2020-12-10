88c80961aee6e62bacac84f8dfa3804f482b4c6c
ouin85 <bouinten@yahoo.fr>
2020-12-10 14:59:44 +0100

    ---

--- app/javascript/src/components/Navbar/SearchBar/index.jsx
+++ app/javascript/src/components/Navbar/SearchBar/index.jsx
@@ -1,71 +1,77 @@
-import React, { useState, useEffect } from 'react'
-import { useHistory } from 'react-router-dom'
-import { Form, FormControl, Button } from 'react-bootstrap'
-import { find, setUrl } from '../../../api/api-manager'
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { Form, FormControl, Button } from 'react-bootstrap';
+import { find, setUrl } from '../../../api/api-manager';
 
 const SearchBar = () => {
-  let history = useHistory()
+  const history = useHistory();
 
-  const [shopCategoriesList, setShopCategoriesList] = useState([])
+  const [shopCategoriesList, setShopCategoriesList] = useState([]);
   const [search, setSearch] = useState({
     keyword: '',
     category: '',
-  })
-  
-  useEffect( () => {
+  });
+
+  useEffect(() => {
     find('shop_categories', {
       onSuccess: (response) => {
-        response?.map( (category) => {
-          setShopCategoriesList(previousArray => [category, ...previousArray])
-        })
-      }
-    })
-  }, []); 
+        response?.map((category) => {
+          setShopCategoriesList((previousArray) => [category, ...previousArray]);
+        });
+      },
+    });
+  }, []);
 
   const handleSearch = () => {
     history.push({
-      pathname: `/itemslist/search`,
-      search:  setUrl('', search)
-    })
-  }
+      pathname: '/itemslist/search',
+      search: setUrl('', search),
+    });
+  };
 
   return (
-  <Form inline>
-    <Form.Control as="select" className='mr-sm-2'>
-      <option
-          value=''
-          key={0}
-          onClick={() => setSearch({...search, category: '' })}
-        >Toute catégorie</option>
-      { shopCategoriesList.map( (cat) => (
+    <Form inline>
+      <Form.Control as="select" className="mr-sm-2">
         <option
-          value={cat.title}
-          key={cat.id}
-          onClick={(event) => setSearch({...search, category: event.target.value })}
+          value=""
+          key={0}
+          onClick={() => setSearch({ ...search, category: '' })}
         >
-          {cat.title}
-        </option>
-      ))}
-    </Form.Control>
-    <FormControl
-      type="text"
-      placeholder="Rechercher un produit"
-      className="mr-sm-2"
-      value={search.keyword}
-      onChange={(event) => setSearch({...search, keyword: event.target.value })}
-      onKeyDown={(event) => {
-        if (event.key === 'Enter') {
-          event.preventDefault()
-          handleSearch(search)
-        }
-      }}
-    />
-    <Button
-      onClick={(event) => handleSearch(search)}
-      variant="outline-success"
-    >Ok</Button>
-  </Form>
-  )
-}
+          Toute catégorie
 
-export default SearchBar
+        </option>
+        { shopCategoriesList.map((cat) => (
+          <option
+            value={cat.title}
+            key={cat.id}
+            onClick={(event) => setSearch({ ...search, category: event.target.value })}
+          >
+            {cat.title}
+          </option>
+        ))}
+      </Form.Control>
+      <FormControl
+        type="text"
+        placeholder="Rechercher un produit"
+        className="mr-sm-2"
+        value={search.keyword}
+        onChange={(event) => setSearch({ ...search, keyword: event.target.value })}
+        onKeyDown={(event) => {
+          if (event.key === 'Enter') {
+            event.preventDefault();
+            handleSearch(search);
+          }
+        }}
+      />
+      <Button
+        onClick={(event) => handleSearch(search)}
+        variant="outline-success"
+      >
+        Ok
+
+      </Button>
+    </Form>
+  );
+};
+
+export default SearchBar;
