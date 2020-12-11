985d314ff5160f919f6a3797df44b40da44954bb
clement <clementpain@outlook.com>
2020-12-11 09:51:41 +0100

    roolback

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,6 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 
-import 'bootstrap'
 import '../src/application.scss'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
