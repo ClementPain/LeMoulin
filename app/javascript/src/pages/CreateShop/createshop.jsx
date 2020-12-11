3b2aad5690ea555eebbee1eeb20273af9e64101d
ouin85 <bouinten@yahoo.fr>
2020-12-11 23:33:56 +0100

    reduce the width of create shop form

--- app/javascript/src/pages/CreateShop/createshop.jsx
+++ app/javascript/src/pages/CreateShop/createshop.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import {
-  Container, Card,
+  Container, Card, Row, Col,
 } from 'react-bootstrap';
 
 import CreateShopFormComponent from '../../components/CreateShopForm';
@@ -9,15 +9,20 @@ import CreateShopFormComponent from '../../components/CreateShopForm';
 const CreateShopComponent = () => (
 
   <Container fluid className="mt-3">
-    <Card>
-      <Card.Body>
-        <h1>Create a shop</h1>
-        <p>
-          Please fill all the fields
-        </p>
-        <CreateShopFormComponent />
-      </Card.Body>
-    </Card>
+    <Row>
+      <Col md={{ span: 8, offset: 2 }}>
+        <Card>
+          <Card.Body>
+            <h1>Create a shop</h1>
+            <p>
+              Please fill all the fields
+            </p>
+            <CreateShopFormComponent />
+          </Card.Body>
+        </Card>
+
+      </Col>
+    </Row>
   </Container>
 
 );
