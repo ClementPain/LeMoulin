03630131802c19ce773c3f70476c38806db10fe0
Nazaninf24 <nazaninf24@gmail.com>
2020-12-15 16:51:49 +0100

    Add

--- app/javascript/src/components/ItemCard/index.jsx
+++ app/javascript/src/components/ItemCard/index.jsx
@@ -1,39 +1,44 @@
-import React from 'react'
-import { Card, Row, Col } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Card, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const ItemCard = ({ item }) => (
-  <Card>
+  <Card className="bg-primary">
     <Card.Img variant="top" />
     <Card.Body>
       <Link
-        className='cardlinks'
+        className="cardlinks"
         to={`shop/${item.shop.id}/item/${item.id}`}
       >
-        <div className='mb-3'>
+        <div className="mb-3 text-white">
           <Card.Title>{ item.name }</Card.Title>
           <Card.Subtitle className="mb-2 text-muted">
-            { item.shop_categories.map( (cat) => cat.title ).join(', ') }
+            { item.shop_categories.map((cat) => cat.title).join(', ') }
           </Card.Subtitle>
           <Card.Text>
-            { item.description.length <= 180 ? item.description : item.description.slice(0, 179) + '...' }
+            { item.description.length <= 180 ? item.description : `${item.description.slice(0, 179)}...` }
           </Card.Text>
         </div>
       </Link>
       <Row>
         <Col sm={9}>
           <footer className="blockquote-footer">
-            <Link 
-              className="cardlinks cardlinks-black"
+            <Link
+              className="cardlinks cardlinks-green"
               to={`shop/${item.shop.id}/item/${item.id}`}
             >
-              {item.shop.name} - {item.shop.address}, {item.shop.city} 
+              {item.shop.name}
+              {' '}
+              -
+              {item.shop.address}
+              ,
+              {item.shop.city}
             </Link>
           </footer>
         </Col>
         <Col sm={3}>
           <Link
-            className='cardlinks cardlinks-green'
+            className="cardlinks cardlinks-green"
             to={`shop/${item.shop.id}/item/${item.id}`}
           >
             Voir plus
@@ -42,6 +47,6 @@ const ItemCard = ({ item }) => (
       </Row>
     </Card.Body>
   </Card>
-)
+);
 
-export default ItemCard
+export default ItemCard;
