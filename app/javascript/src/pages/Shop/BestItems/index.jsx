4bd4647c039f9f2d8bccfe0d720d229e29a2db14
ouin85 <bouinten@yahoo.fr>
2020-12-17 15:35:59 +0100

    install faker add footer in item card

--- app/javascript/src/pages/Shop/BestItems/index.jsx
+++ app/javascript/src/pages/Shop/BestItems/index.jsx
@@ -1,24 +1,41 @@
+/* eslint-disable camelcase */
 /* eslint-disable react/no-array-index-key */
 /* eslint-disable react/prop-types */
 import React from 'react';
 import { Row, Card, Col } from 'react-bootstrap';
 import { Image } from 'cloudinary-react';
+import faker from 'faker';
+import { Link } from 'react-router-dom';
 
 const BestItems = ({ bestItems }) => {
-  console.log(bestItems);
+  console.log('BestItems');
 
   return (
     <Row className="mt-5">
       {
-        bestItems?.map(({ name, description, images }, indx) => (
-          <Col key={indx} md={4}>
-            <Card>
-              <Card.Img as={Image} variant="top" publicId={images[0]} cloudName="dhtysnpro" />
+        bestItems?.map(({
+          id, name, description, images, shop_id,
+        }) => (
+          <Col key={id} md={4}>
+            <Card className="h-100">
+              <Card.Img
+                as={Image}
+                variant="top"
+                publicId={images[0]}
+                cloudName="dhtysnpro"
+              />
+              <Card.Img
+                variant="top"
+                src={faker.image.image()}
+              />
               <Card.Body>
                 <Card.Title>{name}</Card.Title>
                 <Card.Text>
-                  {description?.substring(0, 100)}
+                  {description?.length <= 150 ? description : `${description?.substring(0, 150)}...`}
                 </Card.Text>
+                <footer>
+                  <Link to={`/shop/${shop_id}/item/${id}`} className="cardlinks cardlinks-green">Voir plus</Link>
+                </footer>
               </Card.Body>
             </Card>
           </Col>
