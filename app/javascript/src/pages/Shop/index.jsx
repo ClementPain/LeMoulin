221699b650c1be4bc3d88884bc8bfe2852acf252
ouin85 <bouinten@yahoo.fr>
2020-12-17 16:32:16 +0100

    --

--- app/javascript/src/pages/Shop/index.jsx
+++ app/javascript/src/pages/Shop/index.jsx
@@ -28,66 +28,68 @@ const Shop = () => {
   );
 
   return (
-    <Container className="mt-4">
-      <Card>
-        <Card.Header style={{ backgroundColor: '#45B5AA' }} className="text-center p-2">
-          <h4 className="text-white">{ shop && shop.name}</h4>
-          <p className="text-white m-0">
-            {
+    <>
+      <Container className="mt-4">
+        <Card>
+          <Card.Header style={{ backgroundColor: '#45B5AA' }} className="text-center p-2">
+            <h4 className="text-white">{ shop && shop.name}</h4>
+            <p className="text-white m-0">
+              {
               shop && shop.shop_categories
-                && shop.shop_categories.map(({ title }) => title).join(' - ')
+              && shop.shop_categories.map(({ title }) => title).join(' - ')
             }
-          </p>
-        </Card.Header>
-        <Jumbotron fluid style={{ backgroundImage: `url(${ShopImage})`, backgroundSize: 'cover', backgroundPosition: 'center center' }}>
-          <div className="display-4 text-center" />
-          <div className="my-4 text-center" />
-        </Jumbotron>
-        <Card.Body className="text-primary">
-          <Card.Title as="h6" className="text-center">Description</Card.Title>
-          <Card.Text className="text-center">
-            {
+            </p>
+          </Card.Header>
+          <Jumbotron fluid style={{ backgroundImage: `url(${ShopImage})`, backgroundSize: 'cover', backgroundPosition: 'center center' }}>
+            <div className="display-4 text-center" />
+            <div className="my-4 text-center" />
+          </Jumbotron>
+          <Card.Body className="text-primary">
+            <Card.Title as="h6" className="text-center">Description</Card.Title>
+            <Card.Text className="text-center">
+              {
               shop && shop.description
                 ? shop.description
                 : 'Shop description'
             }
-          </Card.Text>
-          <Row>
-            <Col className="text-center mt-4">
-              <h6 className="m-0">Shop address :</h6>
-              <p className="m-0">
-                {shop?.address}
-                {' '}
-                {shop?.zip_code}
-              </p>
-            </Col>
-            <Col className="text-center mt-4">
-              <h6 className="m-0">Shop city :</h6>
-              <p className="m-0">{shop?.city}</p>
-            </Col>
-            <Col className="text-center mt-4">
-              <h6 className="m-0">References :</h6>
-            </Col>
-          </Row>
+            </Card.Text>
+            <Row>
+              <Col className="text-center mt-4">
+                <h6 className="m-0">Shop address :</h6>
+                <p className="m-0">
+                  {shop?.address}
+                  {' '}
+                  {shop?.zip_code}
+                </p>
+              </Col>
+              <Col className="text-center mt-4">
+                <h6 className="m-0">Shop city :</h6>
+                <p className="m-0">{shop?.city}</p>
+              </Col>
+              <Col className="text-center mt-4">
+                <h6 className="m-0">References :</h6>
+              </Col>
+            </Row>
 
-          { currentUserId === shop?.shopkeeper_id && (
+            { currentUserId === shop?.shopkeeper_id && (
             <Col className="text-center mt-4">
               <Button as={Link} to={`/shop/${id}/list_items`} className="btn btn_success_sass" variant="outline-success">
                 Voir tous mes produits
               </Button>
             </Col>
-          )}
+            )}
 
-          {
+          </Card.Body>
+        </Card>
+      </Container>
+      {
         currentUserId !== shop?.shopkeeper_id && (
-          <Row className="mt-5">
+          <Container>
             <BestItems bestItems={bestShopItemsList} />
-          </Row>
+          </Container>
         )
       }
-        </Card.Body>
-      </Card>
-    </Container>
+    </>
   );
 };
 
