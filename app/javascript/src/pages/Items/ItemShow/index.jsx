73c080ff3434a65e1fe434e041c7100ce1b5524e
Nazaninf24 <nazaninf24@gmail.com>
2020-12-17 14:33:10 +0100

    Add

--- app/javascript/src/pages/Items/ItemShow/index.jsx
+++ app/javascript/src/pages/Items/ItemShow/index.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import {
-  Col, Row, Container, Button
+  Col, Row, Container, Button, Card,
 } from 'react-bootstrap';
 import { find } from '../../../api/api-manager';
 
@@ -12,7 +12,7 @@ import { UpdateItemButton } from '../../../components/ItemCard/ItemButtons';
 
 const Item = () => {
   const { currentUserId } = useSelector((state) => state);
-  console.log('currentUserId', currentUserId)
+  console.log('currentUserId', currentUserId);
   const { item_id } = useParams();
   const [item, setItem] = useState({});
 
@@ -22,8 +22,8 @@ const Item = () => {
       onError: (error) => console.log(error),
       onErrors: (errors) => console.log(errors),
       onSuccess: (result) => {
-        setItem(result)
-        console.log(item)
+        setItem(result);
+        console.log(item);
       },
     });
   }, []);
@@ -32,16 +32,15 @@ const Item = () => {
     <Container fluid>
       <Row className="m-5">
         <Col sm={5}>
-          <ItemImage item={item} />
+            <ItemImage item={item} style={{ hight: 100 }}/>
         </Col>
         <Col sm={1} />
-        <Col sm={6}>
+        <Col sm={6} className="p-4" style={{ backgroundColor: 'white' }}>
           <ItemInformations item={item} />
         </Col>
       </Row>
-
       { currentUserId === item?.shop?.shopkeeper_id && (
-        <Row className='mt-4'>
+        <Row className="mt-4">
           <UpdateItemButton item={item} />
         </Row>
       )}
